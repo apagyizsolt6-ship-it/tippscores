ff55f4a16423d09b40e39817228a127516f4905c
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-08-30 08:58:59 +0200

    MatchListScreen

--- app/src/main/java/com/example/tippscores/ui/screens/MatchListScreen.kt
+++ app/src/main/java/com/example/tippscores/ui/screens/MatchListScreen.kt
@@ -20,8 +20,8 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ChevronLeft
-import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
@@ -1048,9 +1048,9 @@ private fun DateArrow(
 
                 imageVector =
                     if (left) {
-                        Icons.Default.ChevronLeft
+                        Icons.Default.KeyboardArrowLeft
                     } else {
-                        Icons.Default.ChevronRight
+                        Icons.Default.KeyboardArrowRight
                     },
 
                 contentDescription =
